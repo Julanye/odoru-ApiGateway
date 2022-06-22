56d423f91aed38cdc0a5832143ad4ea9192399eb
JulietteLajoinie <juliette.lajoinie@toulouse.miage.fr>
2022-06-22 19:02:19 +0200

    Création de l'API Gateway

---
+++ src/test/java/com/toulouse/miage/lajoinie/juliette/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toulouse.miage.lajoinie.juliette.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
